81694d4c46d9ac4996e9908e2f6229c1bf12e54a
j2.jeremias <j2.jeremias@gmail.com>
2024-09-11 15:10:51 +1000

    add CRUD - Create and Update

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,9 +1,6 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
@@ -18,6 +15,22 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
+        return await Mediator.Send(new Details.Query{Id = id});
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateActivity(Activity activity)
+    {
+        await Mediator.Send(new Create.Command {Activity = activity});
         return Ok();
     }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+        activity.Id = id;
+        await Mediator.Send(new Edit.Command {Activity = activity});
+        return Ok();
+    }
+
 }
