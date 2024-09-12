47ab5fd60762dd773f77da0b0106f0f882bfc61c
j2.jeremias <j2.jeremias@gmail.com>
2024-09-12 13:45:19 +1000

    Add React List, Detail, Card

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,23 @@
+import React, { act } from 'react';
+import { Grid, List } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+import ActivityList from './ActivityList';
+import ActivityDetails from '../details/ActivityDetails';
+
+interface Props {
+    activities: Activity[];
+}
+
+export default function ActivityDashboard({activities}: Props) {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList activities={activities} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] &&
+                <ActivityDetails activity={activities[0]} />}
+            </Grid.Column>  
+        </Grid>
+    )
+}
