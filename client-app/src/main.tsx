5d6ba701feac6d038ed86da9bcf5c1a74f602652
j2.jeremias <j2.jeremias@gmail.com>
2024-10-01 00:55:46 +1000

    add Error handling

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'semantic-ui-css/semantic.min.css'
-import 'react-calendar/dist/Calendar.css'
-import './app/layout/styles.css'
-import { store, StoreContext } from './app/stores/store'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router/Routes'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+import './app/layout/styles.css';
+import { store, StoreContext } from './app/stores/store';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
