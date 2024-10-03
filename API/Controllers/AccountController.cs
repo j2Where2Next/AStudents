e4b7167f3173abfdb3284f20ba9ec1cbe222d335
j2.jeremias <j2.jeremias@gmail.com>
2024-10-03 15:55:37 +1000

    add auth policy, app wide authentication

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,12 +1,14 @@
 using API.DTOs;
 using API.Services;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+[AllowAnonymous]
 [ApiController]
 [Route("api/[controller]")]
 public class AccountController : ControllerBase
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -3,7 +3,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Domain;
-using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services;
